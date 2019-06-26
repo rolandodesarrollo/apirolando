350508d267191d4258dc847348fc2f3203c551da
jlopezcoderepo <=jlopez.coderepo@gmail.com>
2019-06-26 17:48:43 -0500

    Initial Commit

---
+++ WebApiRolando/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebApiRolando.Models;
+
+namespace WebApiRolando.Controllers
+{
+    public class TestController : ApiController
+    {
+        // GET: Test
+        public List<DataTestVM> GetTestData()
+        {
+            var result = new List<DataTestVM>();
+            result.Add(new DataTestVM() { Text = "Uno", Value = 1 });
+            result.Add(new DataTestVM() { Text = "Dos", Value = 2 });
+            result.Add(new DataTestVM() { Text = "Tres", Value = 3 });
+            return result;
+        }
+
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/GetAllData2")]
+        public List<DataTestVM> GetAllData()
+        {
+            var result = new List<DataTestVM>();
+            result.Add(new DataTestVM() { Text = "Cuatro", Value = 4 });
+            result.Add(new DataTestVM() { Text = "Cinco", Value = 5 });
+            result.Add(new DataTestVM() { Text = "Seis", Value = 6 });
+            return result;
+        }
+    }
+}
---
+++ WebApiRolando/Models/DataTestVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiRolando.Models
+{
+    public class DataTestVM
+    {
+        public string Text { get; set; }
+        public int Value { get; set; }
+    }
+}
