4dcef85bdfc938f5abf3189604f25b4a8a01a3fb
jlopezcoderepo <=jlopez.coderepo@gmail.com>
2019-06-26 20:48:12 -0500

    Web Applitacion

---
+++ AxosnetWebApi/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AxosnetWebApi.Startup))]
+namespace AxosnetWebApi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ WebApiRolando/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiRolando.WebApiApplication" Language="C#" %>
