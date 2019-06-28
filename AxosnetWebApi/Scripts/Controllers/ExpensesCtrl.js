785bcd23d0fb78450d6a2ce5b1e715256c90dc57
jlopezcoderepo <=jlopez.coderepo@gmail.com>
2019-06-27 20:32:24 -0500

    commit expenses

---
+++ AxosnetWebApi/Scripts/Controllers/ExpensesCtrl.js
@@ -0,0 +1,5 @@
+var angularApp = angular.module('axosnetApp', []);
+angularApp.controller('expensesListCtrl', function ($scope, $http) {
+    $scope.message = "Lista de gastos registrados";
+});
+//# sourceMappingURL=ExpensesCtrl.js.map
---
+++ AxosnetWebApi/Scripts/Controllers/ExpensesCtrl.ts
@@ -0,0 +1,19 @@
+﻿var angularApp = angular.module('axosnetApp', []);
+
+interface ExpenseListScope extends ng.IScope {
+    message: string;
+    search();
+    //providers: AxosnetWebApi.Models.ProviderVM[];
+    openAddProviderPopUp();
+    responseMessage: string;
+    addNewProvider();
+    newProviderName: string;
+    newProviderTelephone: string;
+}
+
+angularApp.controller('expensesListCtrl', function ($scope: ProvidersListScope, $http: ng.IHttpService) {
+    $scope.message = "Lista de gastos registrados";
+   
+
+
+});
